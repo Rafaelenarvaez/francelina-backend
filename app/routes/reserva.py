8f4b83fe02749db7ebef9e82b3a4ca4bad2b07b9
rafaelenarvaez <rafaelnarvaez1510@gmail.com>
2023-07-26 10:09:34 -0400

    produccion

--- app/routes/reserva.py
+++ app/routes/reserva.py
@@ -128,14 +128,14 @@ async def reserva(
 
     conn.execute(reservas.insert().values(new_reserve))
 
-    # message = MessageSchema(
-    #     subject="Confirmacion de reserva",
-    #     recipients=[reserva.email, credenciales["EMAIL"]],
-    #     template_body=new_reserve,
-    # )
+    message = MessageSchema(
+         subject="Confirmacion de reserva",
+         recipients=[reserva.email, credenciales["EMAIL"]],
+        template_body=new_reserve,
+    )
 
-    # fm = FastMail(conf)
-    # await fm.send_message(message, template_name="email.html")
+    fm = FastMail(conf)
+    await fm.send_message(message, template_name="email.html")
 
     return {"message": "reserva creada exitosamente"}
 
