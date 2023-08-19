3a8ad913ea458e542350cc8f730dde19aede6c40
sdrosa <samuel@techandpeople.com>
2023-08-19 18:16:24 -0400

    reservation fix

--- app/config/db.py
+++ app/config/db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine, MetaData
 import databases
 
 
-engine = create_engine("mysql+pymysql://root@localhost:3306/francelina")
+engine = create_engine("mysql+pymysql://root@localhost:3306/francelina_dev")
 
 
 meta = MetaData()
