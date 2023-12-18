d9a44d87eeacd2eb4f131a7660b781a6fb044b63
srosa <srosa.dev@gmail.com>
2023-12-18 00:08:56 -0400

    feature: menu dishes

--- app/routes/create_menu.py
+++ app/routes/create_menu.py
@@ -175,10 +175,118 @@ async def update_menu(
 
 
 
+@create_menu.post("/menu")
+async def create_categorias(
+    menu:Menu,
+    ##current_user: Any = Security(get_current_active_user)
+):
+    new_categoria={
+        "nombre": menu.nombre.lower(),
+        "descripcion": menu.descripcion.lower()
+    }
+    result =conn.execute(menu1.insert().values(new_categoria))
+    return "success"
+
+@create_menu.post("/create_platillo")
+async def create( 
+    categoria: str,
+    nombre: str,
+    precio: str,
+    descripcion: str,
+    ##current_user: Any = Security(get_current_active_user)
+):
+
+    qr = menu1.select().where(menu1.c.nombre == categoria.lower())
+    menu = conn.execute(qr).fetchone()
+    
+    if not menu:
+         return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                'msg': 'Esta categoria no existe'
+            })
+
+    qr = platillos.select().where(platillos.c.categoria_id == menu['id'])
+
+    new_menu={
+        "nombre": nombre, 
+        "precio": precio,
+        "descripcion": descripcion, 
+        "categoria_id": menu['id']
+    }
+
+    new_dish_id = conn.execute(platillos.insert().values(new_menu)).inserted_primary_key[0]
+    new_dish = conn.execute(platillos.select().where(platillos.c.id == new_dish_id)).fetchone()
+   
+    
+    return new_dish
+
+
+@create_menu.get("/platos")
+def get_platos(
+    nested: bool,
+    paginate: bool,
+    skip: int = 0,
+    limit: int = 100,
+):
+    if nested:
+        if paginate:
+            query = menu1.select().offset(skip).limit(limit)
+        else:
+            query = menu1.select()
+
+        menu = conn.execute(query).fetchall()
+
+        if not menu:
+            return []
+        
+        menu_lst = []
+
+        for r in menu:
+
+            dishes = conn.execute(platillos.select().where(platillos.c.categoria_id == r['id']))
+            
+            if dishes:
+                dish_obj = {**dict(r), 'platillos': [{**dict(i)} for i in dishes]}
+            else:
+                dish_obj = {**dict(r), 'platillos': []}
+
+            menu_lst.append(dish_obj)
+
+        return menu_lst
+    
+    else:
+        if paginate:
+            query = platillos.select().order_by(desc(platillos.c.id)).offset(skip).limit(limit)
+        else:
+            query = platillos.select().order_by(desc(platillos.c.id))
+        return conn.execute(query).fetchall()
+
+
+@create_menu.put("/admin/update_menu")
+async def update_menu(
+    ides: int,
+    nombre: str,
+    precio: str,
+    descripcion: str,
+   ## current_user: Any = Security(get_current_active_user)
+):
+    new_menu = {
+        "nombre": nombre, 
+        "precio": precio,
+        "descripcion": descripcion, 
+    }
+
+    rp = conn.execute(platillos.update().values(new_menu).where(platillos.c.id == ides))
+
+    
+    return rp
+
+
 @create_menu.delete("/admin/delate")
 def borrar_categoria(
     id:str,
-    current_user: Any = Security(get_current_active_user)
+   ## current_user: Any = Security(get_current_active_user)
 ):
     conn.execute(menu1.delete().where(menu1.c.id == id))
     return { 'message': "success"}
@@ -187,7 +295,7 @@ def borrar_categoria(
 @create_menu.delete("/platos/delete")
 def borrar_plato(
     id: int,
-    current_user: Any = Security(get_current_active_user)
+   ## current_user: Any = Security(get_current_active_user)
 ):
     conn.execute(platillos.delete().where(platillos.c.id == id))
     return { 'message': "success" }
