7d848b5d4f10e1db030386e649dc0a8a79082ca2
srosadev1~ <srosa@aluxion.com>
2024-04-24 18:41:28 -0400

    restoring

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.router import api_router
 # from config.db import db
@@ -9,7 +9,18 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 load_dotenv() 
+origins = [
+    "https://francelina.co",
+    "https://www.francelina.co",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router, prefix='/api')
 
 
